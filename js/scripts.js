0903a6e042c01da08b770d968a5615645c742c38
chris breaux and omar kadah <chris@>
2019-01-24 09:36:12 -0800

    setup intial files and directories, build out intial two conditions

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// var leapYear = function(year) {
+//   return false;
+// }
+
+function leapYear(year) {
+	if (year % 4 === 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+		console.log("submitted")
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
