e527dd0c8f1d4aaba3c25ee2373c9a00ace87ce6
chris breaux and omar kadah <chris@>
2019-01-24 09:44:15 -0800

    refactor script

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,12 @@
 //   return false;
 // }
 
-function leapYear(year) {
-	if (year % 4 === 0) {
-		return true;
-	} else {
-		return false;
-	}
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 
