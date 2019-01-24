39f61a8d0db519dd617afc2a2041cac4d9bec20c
chris breaux and omar kadah <chris@>
2019-01-24 09:50:10 -0800

    refactor scripts again using alternate function naming convention

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 //   return false;
 // }
 
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+function leapYear(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
